50089996f145d37ec1e1799aabd37195c1383ea6
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2022-04-05 11:23:21 +0800

    Add update:readme command

---
+++ app/Console/Commands/UpdateReadmeCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+class UpdateReadmeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:readme';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update README.md file';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $filesystem = new Filesystem();
+        $readme_template = $filesystem->get(storage_path('readme-template.dat'));
+
+        $today = today();
+        $content = str_replace('%today-wallpaper%', "![{$today->toDateString()}](./storage/bing-wallpaper/{$today->year}/{$today->format('m')}/{$today->toDateString()}.png)" . PHP_EOL . $today->toDateString(), $readme_template);
+
+        $filesystem->put(base_path('README.md'), $content);
+    }
+}
